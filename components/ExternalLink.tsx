98e1fe2859cc679a4ea7e768e4975b733d167de3
Geonhee Lee <me@szk.kr>
2025-10-17 10:00:30 +0900

    fix: dependency, config

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,10 +1,12 @@
 import { Link } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import * as React from 'react';
 import { Platform } from 'react-native';
 
+type ExternalLinkProps = Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string };
+
 export function ExternalLink(
-  props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }
+  props: ExternalLinkProps
 ) {
   return (
     <Link
